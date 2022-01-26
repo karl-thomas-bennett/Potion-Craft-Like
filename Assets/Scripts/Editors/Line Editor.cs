7785a2b0054e55ac19aad6cd302155cc15ca05b7
Karl Bennett <karlthomasbennett@gmail.com>
2022-01-26 19:59:09 +1300

    Add position handles to points on line

--- Assets/Scripts/Editors/Line Editor.cs
+++ Assets/Scripts/Editors/Line Editor.cs
@@ -11,11 +11,21 @@ public class LineEditor : Editor
     private void OnEnable()
     {
         points = serializedObject.FindProperty("points");
+        
     }
-    public override void OnInspectorGUI()
+    /*public override void OnInspectorGUI()
     {
         serializedObject.Update();
         EditorGUILayout.PropertyField(points);
         serializedObject.ApplyModifiedProperties();
+    }*/
+
+    private void OnSceneGUI()
+    {
+        Line line = (Line)target;
+        for(int i = 0; i < line.points.Count; i++)
+        {
+            line.points[i] = Handles.PositionHandle(new Vector3(line.points[i].x, line.points[i].y, line.transform.position.z), Quaternion.identity);
+        }
     }
 }
